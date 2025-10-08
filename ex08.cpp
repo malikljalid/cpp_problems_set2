2753c3cc8c77a6a17bbd6f05b06e7f84de4028a8
Zakaria Adriouche <zakariaadriouche@gmail.com>
2025-10-08 13:00:59 +0100

    get the frequency of a digit in a number

---
+++ ex08.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+int readNumber(void)
+{
+    int Number;
+
+    std::cout << "Please enter a number : ";
+    std::cin >> Number;
+
+    return (Number);
+}
+
+int readDigit(void)
+{
+    int Digit;
+
+    std::cout << "Please enter a digit : ";
+    std::cin >> Digit;
+
+    return (Digit);
+}
+
+int LastDigitOf(int Number)
+{
+    return (Number % 10);
+}
+
+int getDigitFrequencyInNumber(int Digit, int Number)
+{
+    int Remainder  = Number;
+    int Frequency  = 0;
+
+    while (Remainder != 0)
+    {
+        if (LastDigitOf(Remainder) == Digit)
+            Frequency++;
+        Remainder /= 10;
+    }
+    return (Frequency);
+}
+
+void printNumberSumOfDigits(int Digit, int Number)
+{
+    std::cout << Digit << " Frequency in " << Number << " is : " << getDigitFrequencyInNumber(Digit, Number) << " Times\n";
+}
+
+int main(void)
+{
+    printNumberSumOfDigits(readDigit(), readNumber());
+    return (0);
+}
