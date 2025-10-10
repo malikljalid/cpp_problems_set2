c5877f2c6eb1b032ae511335548fe960a71adf7d
Zakaria Adriouche <zakariaadriouche@gmail.com>
2025-10-10 10:19:38 +0100

    get max number in array

---
+++ ex24.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+enum enDefaultSize { MAXSIZE = 100 };
+
+int randomNumber(int From, int To)
+{
+    return ((rand() % To - From + 1) - From);
+}
+
+void fillArrayElementsWithRandomValues(int arr[MAXSIZE], int &Size, int From, int To)
+{
+    std::cout << "How many elements you want in the array ? : ";
+    std::cin >> Size;
+
+    for (int i = 0; i < Size; i++)
+    {
+        arr[i] = randomNumber(From, To);
+    }
+}
+
+void printArrayElements(int arr[MAXSIZE], int Size)
+{
+    std::cout << "\nOriginal Array : ";
+    for (int i = 0; i < Size; i++)
+    {
+        std::cout << arr[i] << ' ';
+    }
+    std::cout << '\n';
+}
+
+int getMaxNumberInArray(int arr[MAXSIZE], int Size)
+{
+    int max = arr[0];
+
+    for (int i = 0; i < Size; i++)
+    {
+        if (arr[i] > max)
+            max = arr[i];
+    }
+    return (max);
+}
+
+int main(void)
+{
+    srand((unsigned)time(NULL));
+
+    int arr[MAXSIZE];
+    int arrSize = 0;
+
+    fillArrayElementsWithRandomValues(arr, arrSize, 1, 100);
+    printArrayElements(arr, arrSize);
+
+    std::cout << "Max Number is : " << getMaxNumberInArray(arr, arrSize) << '\n';
+
+    return (0);
+}
