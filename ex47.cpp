02c16153372d6c78de798f66530b2f00dd69941a
Zakaria Adriouche <zakariaadriouche@gmail.com>
2025-10-12 19:15:16 +0100

    my round vs their round.. and yes its more intelcual

---
+++ ex47.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <cmath>
+
+float readNumber(void)
+{
+    float Number;
+
+    std::cout << "Please enter a number : ";
+    std::cin >> Number;
+
+    return (Number);
+}
+
+int myAbs(int Number)
+{
+    if (Number < 0)
+        return (Number * -1);
+    else
+        return (Number);
+}
+
+int Last4DigitsOf(int Number)
+{
+    return (Number % 1000);
+}
+
+float getFractionPartOf(float Number)
+{
+    int FractionPart = myAbs((int)(Number * 1000));
+
+    return (Last4DigitsOf(FractionPart) / (float)1000);
+}
+
+float myRound(float Number)
+{
+    float fractionPart = getFractionPartOf(Number);
+
+    if (Number >= 0 && fractionPart < 0.5)
+        return (Number - fractionPart);
+    else if (Number >= 0 && fractionPart >= 0.5)
+        return (Number - fractionPart + 1);
+    else if (Number <  0 && fractionPart >= 0.5)
+        return (Number + fractionPart - 1);
+    else
+        return (Number + fractionPart);
+}
+
+int main(void)
+{
+    float Number = readNumber();
+
+    std::cout << "My abs  : " << myRound(Number) << '\n';
+    std::cout << "C++ abs : " << round(Number) << '\n';
+
+    return (0);
+}
