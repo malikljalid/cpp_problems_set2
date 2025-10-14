0ae113e0c8bfbe593385b0b1095f372aa14e6aab
Zakaria Adriouche <zakariaadriouche@gmail.com>
2025-10-14 11:48:46 +0100

    make the struct of the math game

---
+++ project2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+enum    enOperationsLevel    { EASY, MED, HARD, MIX };
+enum    enOperationsType     { ADD='+', SUB='-', MUL='*', DIV='/', ALL };
+enum    enOperationsResult   { FAIL, PASS };
+
+struct  stOperations
+{
+    enOperationsLevel       Level;
+    enOperationsType        Type;
+    enOperationsResult      FinalResult;
+    short int               NumberOfQuestions;
+    short int               WrongAnswers;
+    short int               RightAnswers;
+    char                    playAgain;
+};
+
+int randomNumber(int From, int To)
+{
+    return ((rand() % (To - From + 1)) + From);
+}
+
+int main(void)
+{
+    srand((unsigned int)time(NULL));
+    return (0);
+}
